32aadf5b357fdb519120e644b996c3b6fe8473eb
rupakolee <oleerupak11@gmail.com>
2026-08-29 16:45:18 +0545

    set dbName if database doesn't already exist

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "stock-price-predictor" });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error", err);
